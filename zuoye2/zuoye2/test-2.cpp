1211c7bf45ecf90e23c931ff8998ef7c95229af8
XIRANSI <2461468217@qq.com>
2019-11-17 17:55:20 +0800

    用c++写得哈夫曼树

--- zuoye2/zuoye2/test-2.cpp
+++ zuoye2/zuoye2/test-2.cpp
@@ -5,9 +5,9 @@ int main()
 	int a=1;
 	int b=2;
 	int c=0;
-	printf("a=%d b=%d\n", a, b);
+	printf("a=%d b=%d\n"
+	a = b;, a, b);
 	c = a;
-	a = b;
 	b = c;
 	printf("a=%d b=%d\n", a, b);
         return 0;
